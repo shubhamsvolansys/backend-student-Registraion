acb8de37403904b4f6cd72df62acef301e733a56
shubhamsvolansys <shubham.sharma@volansys.com>
2022-09-13 13:21:14 +0530

    after router created

--- src/app.js
+++ src/app.js
@@ -21,66 +21,10 @@ app.use(studentRouter);
 //     })
 // })
 
-app.post("/students", async(req,res)=>{
-    try{
-        const user= new Students(req.body);
-        const createuser = await user.save();
-        res.status(201).send(createuser);
-    }catch(e){
-        res.status(400).send(e);
 
-    }
-})
-
-app.get("/students", async(req, res)=>{
-    try{
-     const studentsData = await Students.find();
-        res.send(studentsData);
-    }
-    catch(err){
-        console.log(err);
-    }
-    })
-
-app.get("/students/:id", async(req, res)=>{
-    try{
-            const id = req.params.id;
-         const studentsData = await Students.findById(id);
-         if(!studentsData) return res.status(404).send();
-            res.send(studentsData);
-        }
-    catch(err){
-            console.log(err);
-        }
-        })
-
-/**
- * Update students details
- */
-app.patch("/student/:id", async(req,res)=>{
-    try{
-            const _id = req.params.id;
-        const Studentupdate = await Students.findByIdAndUpdate(_id, req.body);
-        res.status(201).send(Studentupdate);
-        console.log(req.body);
-
-    }catch(err){
-        console.log(err);
-    }
-})
-
-app.delete("/student/:id", async(req,res)=>{
-    try{
-       const Deleteduser = await Students.findByIdAndDelete(req.params.id);
-       if(!req.params.id) return res.status(400).send();
-       res.send(deleteStudent);
-
-    }catch(e){
-        res.status(500).send(e);
-    }
-})
 
 
 app.listen(port, ()=>{
     console.log("connection is set up in port number ", port); 
 })
+ 
--- src/router/student.js
+++ src/router/student.js
@@ -1,8 +1,63 @@
 const express = require("express");
 const router = new express.Router();
+const Students = require("../modals/students");
 
-router.get("/hello", (req,res)=>{
-    res.send("Hello from my side");
+router.post("/students", async(req,res)=>{
+    try{
+        const user= new Students(req.body);
+        const createuser = await user.save();
+        res.status(201).send(createuser);
+    }catch(e){
+        res.status(400).send(e);
+
+    }
 })
 
+router.get("/students", async(req, res)=>{
+    try{
+     const studentsData = await Students.find();
+        res.send(studentsData);
+    }
+    catch(err){
+        console.log(err);
+    }
+    })
+
+router.get("/students/:id", async(req, res)=>{
+    try{
+            const id = req.params.id;
+         const studentsData = await Students.findById(id);
+         if(!studentsData) return res.status(404).send();
+            res.send(studentsData);
+        }
+    catch(err){
+            console.log(err);
+        }
+        })
+
+/**
+ * Update students details
+ */
+router.patch("/student/:id", async(req,res)=>{
+    try{
+            const _id = req.params.id;
+        const Studentupdate = await Students.findByIdAndUpdate(_id, req.body);
+        res.status(201).send(Studentupdate);
+        console.log(req.body);
+
+    }catch(err){
+        console.log(err);
+    }
+})
+
+router.delete("/student/:id", async(req,res)=>{
+    try{
+       const Deleteduser = await Students.findByIdAndDelete(req.params.id);
+       if(!req.params.id) return res.status(400).send();
+       res.send(deleteStudent);
+
+    }catch(e){
+        res.status(500).send(e);
+    }
+})
 module.exports = router;
